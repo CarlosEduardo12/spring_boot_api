b5aa55298d0c967723516cd539060427e302d09d
Carlos <carlosdudujunior@hotmail.com>
2020-08-13 13:22:16 -0300

    definindo nossa entidade

--- src/main/java/br/com/carloseduardo/apirest/model/Livro.java
+++ src/main/java/br/com/carloseduardo/apirest/model/Livro.java
@@ -1,2 +1,43 @@
-package br.com.carloseduardo.apirest.model;public class Livro {
+package br.com.carloseduardo.apirest.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "autor")
+    private String autor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 }
