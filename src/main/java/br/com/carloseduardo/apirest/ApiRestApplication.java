0092b896ff5b624961dac46a1a0af2c4dee8d1fb
Carlos <carlosdudujunior@hotmail.com>
2020-08-13 13:23:24 -0300

    definindo padrões de inicialização

--- src/main/java/br/com/carloseduardo/apirest/ApiRestApplication.java
+++ src/main/java/br/com/carloseduardo/apirest/ApiRestApplication.java
@@ -2,8 +2,10 @@ package br.com.carloseduardo.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.carloseduardo.apirest")
+@EntityScan(basePackages = "br.com.carloseduardo.apirest.model")
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
