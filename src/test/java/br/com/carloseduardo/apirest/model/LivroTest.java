cab80d36dce0613a8abb49f72ec747f80293be5a
Carlos <carlosdudujunior@hotmail.com>
2020-08-13 14:01:50 -0300

    teste

--- src/test/java/br/com/carloseduardo/apirest/model/LivroTest.java
+++ src/test/java/br/com/carloseduardo/apirest/model/LivroTest.java
@@ -1,4 +1,12 @@
+package br.com.carloseduardo.apirest.model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LivroTest {
-  
+
+    @Test
+    void getId() {
+    }
 }
